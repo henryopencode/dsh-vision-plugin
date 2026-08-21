1bd457253cbb91a9cf6d3b9f3e561b1ce15d3deb
henry <henry@noreply.gitee.com>
2026-08-22 00:02:53 +0800

    fix: rail padding-bottom 10px

--- browser/dsh-client-ui-vision-bridge/lib/client.js
+++ browser/dsh-client-ui-vision-bridge/lib/client.js
@@ -1059,7 +1059,7 @@ window.__ModuleLoader__.load({
 							"justify-content:flex-start",
 							"gap:6px",
 							"flex-wrap:nowrap",
-							"padding:0 12px",
+							"padding:0 12px 10px",
 							"min-height:30px",
 							"margin-top:-10px",
 							"margin-bottom:-12px",
